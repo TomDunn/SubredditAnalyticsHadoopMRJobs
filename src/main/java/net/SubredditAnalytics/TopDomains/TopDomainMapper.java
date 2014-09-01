983d8e014ddc29a837230fddfb39326035e29377
tomdunn <tidunn13@gmail.com>
2014-09-01 13:02:06 -0700

    catch array out of bounds for bad input lines

--- src/main/java/net/SubredditAnalytics/TopDomains/TopDomainMapper.java
+++ src/main/java/net/SubredditAnalytics/TopDomains/TopDomainMapper.java
@@ -11,15 +11,19 @@ import java.io.IOException;
  */
 public class TopDomainMapper extends Mapper<LongWritable, Text, Text, Text> {
     public void map(LongWritable k, Text value, Context context) throws InterruptedException, IOException {
-        final String[] columns = value.toString().split("\t+");
+        try {
+            final String[] columns = value.toString().split("\t+");
 
-        final String key = columns[0];
+            final String key = columns[0];
 
-        final String subreddit = key.split("\\|")[0];
-        final String domain    = key.split("\\|")[1];
-        final int domainCount  = Integer.parseInt(columns[1]);
+            final String subreddit = key.split("\\|")[0];
+            final String domain = key.split("\\|")[1];
+            final int domainCount = Integer.parseInt(columns[1]);
 
-        context.write(new Text(subreddit), new Text(domain + "|" + Integer.toString(domainCount)));
-        context.write(new Text(subreddit), new Text("_TOTAL_|" + Integer.toString(domainCount)));
+            context.write(new Text(subreddit), new Text(domain + "|" + Integer.toString(domainCount)));
+            context.write(new Text(subreddit), new Text("_TOTAL_|" + Integer.toString(domainCount)));
+        } catch (ArrayIndexOutOfBoundsException boundsException) {
+            // silently, todo: error logging here.
+        }
     }
 }
