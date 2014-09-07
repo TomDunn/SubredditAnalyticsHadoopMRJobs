bd4e6f02307e048d3bb67135336cb6d59792bc0b
tomdunn <tidunn13@gmail.com>
2014-09-07 10:39:00 -0700

    chain all jobs to run together

--- src/main/java/net/SubredditAnalytics/MRJobRunner.java
+++ src/main/java/net/SubredditAnalytics/MRJobRunner.java
@@ -9,6 +9,8 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -19,8 +21,14 @@ import java.util.Map;
  */
 public class MRJobRunner {
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
         if (args.length < 3) {
             throw new IllegalArgumentException("Three arguments are required: JobName InPath OutPath");
+        }
+
+        if (args[0].equals("RUN_ALL")) {
+            MRJobRunner.runAll(new Path(args[1]), new Path(args[2]));
+            return;
         }
 
         final Map<String, MRJobFactory> jobNameToFactory = new HashMap<String, MRJobFactory>();
@@ -42,5 +50,48 @@ public class MRJobRunner {
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
         job.waitForCompletion(true);
+    }
+
+    public static void runAll(final Path inPath, final Path baseOutPath) throws ClassNotFoundException, InterruptedException, IOException {
+        DateTime dateTime = new DateTime(DateTimeZone.UTC);
+
+        final String nowTimestamp = Long.toString(dateTime.now().getMillis() / 1000L);
+        final Path baseJobOutPath = new Path(baseOutPath.toString() + "/" + nowTimestamp);
+
+        /* Unique Post Job */
+        Job uniquePostJob = (new UniquePostFilterJobFactory()).makeMapReduceJob();
+        Path uniquePostOutPath = new Path(baseJobOutPath.toString() + "/" + "UNIQUE_POSTS");
+
+        FileInputFormat.addInputPath(uniquePostJob, inPath);
+        FileOutputFormat.setOutputPath(uniquePostJob, uniquePostOutPath);
+
+        uniquePostJob.waitForCompletion(true);
+
+        /* Hourly post count jobs */
+        Job hourlyPostCountJob = (new HourlyPostCountJobFactory()).makeMapReduceJob();
+        Path hourlyPostCountOutPath = new Path(baseJobOutPath.toString() + "/HOURLY_POST_COUNTS");
+
+        FileInputFormat.addInputPath(hourlyPostCountJob, uniquePostOutPath);
+        FileOutputFormat.setOutputPath(hourlyPostCountJob, hourlyPostCountOutPath);
+
+        hourlyPostCountJob.waitForCompletion(true);
+
+        /* Domain counts */
+        Job postDomainCountJob = (new SubredditLinkDomainCountJobFactory()).makeMapReduceJob();
+        Path postDomainCountOutPath = new Path(baseJobOutPath.toString() + "/DOMAIN_COUNTS");
+
+        FileInputFormat.addInputPath(postDomainCountJob, uniquePostOutPath);
+        FileOutputFormat.setOutputPath(postDomainCountJob, postDomainCountOutPath);
+
+        postDomainCountJob.waitForCompletion(true);
+
+        /* Top Domains */
+        Job topDomainJob = (new TopDomainJobFactory()).makeMapReduceJob();
+        Path topDomainOutPath = new Path(baseJobOutPath.toString() + "/TOP_DOMAINS");
+
+        FileInputFormat.addInputPath(topDomainJob, postDomainCountOutPath);
+        FileOutputFormat.setOutputPath(topDomainJob, topDomainOutPath);
+
+        topDomainJob.waitForCompletion(true);
     }
 }
