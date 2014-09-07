19112a67518a993cf31f25b5c3a6f9f38415ded1
tomdunn <tidunn13@gmail.com>
2014-09-07 16:04:36 -0700

    remove unused import

--- src/main/java/net/SubredditAnalytics/HourlyPostCount/HourlyPostCountJobFactory.java
+++ src/main/java/net/SubredditAnalytics/HourlyPostCount/HourlyPostCountJobFactory.java
@@ -2,7 +2,6 @@ package net.SubredditAnalytics.HourlyPostCount;
 
 import net.SubredditAnalytics.Jobs.MRJobFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
